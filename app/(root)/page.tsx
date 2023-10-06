0aa6f7ab6b30e070e6189a1ad57985ae03b41829
vernierhawk <prsaxena007@gmail.com>
2023-10-06 20:06:44 +0530

    init

---
+++ app/(root)/page.tsx
@@ -0,0 +1,13 @@
+import { Button } from "@/components/ui/button"
+
+export default function Home() {
+    return (
+    <div>
+      
+      <Button variant="default ">Click Me</Button>
+      <h1 className="text-center">Hello</h1>
+      
+    </div>
+    )
+  }
+  
